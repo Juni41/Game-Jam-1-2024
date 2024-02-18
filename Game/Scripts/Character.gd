229874e983073b51e11c6fba555a48033da465e4
Juni41 <hsiaojustin41@gmail.com>
2024-02-18 17:40:59 +1300

    ghgh

--- Game/Scripts/Character.gd
+++ Game/Scripts/Character.gd
@@ -4,10 +4,12 @@ extends CharacterBody2D
 @export var speed : float = 150
 @export var jump_velocity : float = -200.0
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+var animation_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,10 +21,17 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("Left", "Right")
+	direction = Input.get_vector("Left", "Right", "up", "down")
+	
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	update_animation()
 	move_and_slide()
+func update_animation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("walk")
+		else:
+			animated_sprite.play("idle")
