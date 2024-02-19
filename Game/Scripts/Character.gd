cc96aa9f4b74439339157184c1790a1b0cbe3230
Juni41 <hsiaojustin41@gmail.com>
2024-02-20 12:21:56 +1300

    adqwd

--- Game/Scripts/Character.gd
+++ Game/Scripts/Character.gd
@@ -3,29 +3,22 @@ extends CharacterBody2D
 @export var SPEED : int = 100
 @export var JUMP_FORCE : int = 175
 @export var GRAVITY : int = 750
- 
+
+
+
+
 func _physics_process(delta):
-	
-	
 	var direction = Input.get_axis("Left","Right")
-	
 	if direction:
-		
 		velocity.x = SPEED * direction
-		
 		if is_on_floor():
-			
 			$AnimatedSprite2D.play("run")
-		
 	else:
-		
 		velocity.x = 0
-		
 		if is_on_floor():
-			
 			$AnimatedSprite2D.play("idle")
 	
-	# Rotate
+	# flips character
 	
 	if direction == 1:
 		$AnimatedSprite2D.flip_h = false
@@ -54,4 +47,5 @@ func _physics_process(delta):
 	
 	
 	move_and_slide()
- 
+
+	
