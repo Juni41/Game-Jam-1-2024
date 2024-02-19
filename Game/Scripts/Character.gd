c200055716e1bfc3a8e5ca14db06dbc19269bfff
Juni41 <hsiaojustin41@gmail.com>
2024-02-19 18:33:52 +1300

    revamped movement script
    old script shatitself so now it has jump

--- Game/Scripts/Character.gd
+++ Game/Scripts/Character.gd
@@ -1,45 +1,57 @@
 extends CharacterBody2D
-
-
-@export var speed : float = 150
-@export var jump_velocity : float = -200.0
-
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var animation_locked : bool = false
-var direction : Vector2 = Vector2.ZERO
+ 
+@export var SPEED : int = 100
+@export var JUMP_FORCE : int = 175
+@export var GRAVITY : int = 750
+ 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_vector("Left", "Right", "up", "down")
+	
+	
+	var direction = Input.get_axis("Left","Right")
 	
 	if direction:
-		velocity.x = direction.x * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-	update_animation()
-	move_and_slide()
-	update_direction_h()
-func update_animation():
-	if not animation_locked:
-		if direction.x != 0:
-			animated_sprite.play("walk")
 		
-		else:
-			animated_sprite.play("idle")
-		   
-func update_direction_h():
-	if direction.x > 0:
-		animated_sprite.flip_h = false
-	elif direction.x < 0:
-		animated_sprite.flip_h = true
+		velocity.x = SPEED * direction
+		
+		if is_on_floor():
+			
+			$AnimatedSprite2D.play("run")
+		
+	else:
+		
+		velocity.x = 0
+		
+		if is_on_floor():
+			
+			$AnimatedSprite2D.play("idle")
+	
+	# Rotate
+	
+	if direction == 1:
+		$AnimatedSprite2D.flip_h = false
+	elif direction == -1:
+		$AnimatedSprite2D.flip_h = true
+		
+	
+	# Gravity
+	
+	if not is_on_floor():
+		
+		velocity.y += GRAVITY * delta
+		
+		if velocity.y > 0:
+			
+			$AnimatedSprite2D.play("Fall")
+	
+	# Jump
+	
+	if is_on_floor():
+		
+		if Input.is_action_just_pressed("jump"):
+			
+			velocity.y -= JUMP_FORCE
+			$AnimatedSprite2D.play("jump")
+	
+	
+	move_and_slide()
+ 
