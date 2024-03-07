1443aa6cfcc5b682def601a86d96cfdcb9e01b3b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-08 09:09:05 +1300

    Finished current menu

--- Game/Magik/options_menu.gd
+++ Game/Magik/options_menu.gd
@@ -3,3 +3,10 @@ extends Control
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Magik/menu.tscn")
+
+
+func _on_fullscreen_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
