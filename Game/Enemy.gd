03866860f091861a28fb62e28208d6c666ec897f
Tanush101 <140560214+Tanush101@users.noreply.github.com>
2024-03-15 00:16:26 +1300

    changed enemy ai a bit

--- Game/Enemy.gd
+++ Game/Enemy.gd
@@ -5,13 +5,16 @@ var player_chase = false
 var player = null
 
 func _physics_process(delta):
-	if player_chase:
-		position+= (player.position - position) / speed
+	if player_chase and player:
+		var direction = (player.position - position).normalized()
+		position += direction * speed * delta
 
 func _on_detection_area_body_entered(body):
-	player = body
-	player_chase = true
+	if body.is_in_group("player"):
+		player = body
+		player_chase = true
 
 func _on_detection_area_body_exited(body):
-	player = null
-	player_chase = false
+	if body == player:
+		player = null
+		player_chase = false
