8ecb0019e7a1b7017de791c37783b86419b39cab
Juni41 <hsiaojustin41@gmail.com>
2024-03-14 21:32:46 +1300

    i hate my life

--- Game/main.gd
+++ Game/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var pause_menu = $PauseMenu
 var paused = false
-update_dirty_quadrants()
+
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
