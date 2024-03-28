18bcf9edec909ce099888a05d94d8f1901b0826f
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-28 14:27:50 +1300

    comments for menu

--- Game/Menu.gd
+++ Game/Menu.gd
@@ -1,13 +1,13 @@
 extends Control
 
 
-func _on_play_pressed():
+func _on_play_pressed():#hide menu and initiate game
 	get_tree().change_scene_to_file("res://main.tscn")
 
 
-func _on_options_pressed():
+func _on_options_pressed():#switch scenes to option menu
 	get_tree().change_scene_to_file("res://Magik/options_menu.tscn")
 
 
 func _on_quit_pressed():
-	get_tree().quit()
+	get_tree().quit()#close window/end task
